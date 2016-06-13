e34a39caac0eff7065a632cc03f3f1ec6646953d
torston <turushev.dmitriy@gmail.com>
2016-06-14 02:26:29 +0300

    refactoring

--- app/buildings.controller.ts
+++ app/buildings.controller.ts
@@ -43,13 +43,11 @@ class BuildingsController {
 
         var intersects = this.raycaster.intersectObjects(this.r.scene.children);
 
-        if (intersects.length == 0) {
+        if (intersects.length == 0 || intersects == []) {
             return;
         }
 
-        console.log(intersects.length);
-
-        let p = intersects[0].point;
+        let firstIntersection = intersects[0].point;
 
         for (var i = 0; i < intersects.length; i++) {
             var element: THREE.Object3D = intersects[i].object;
@@ -67,85 +65,55 @@ class BuildingsController {
         var sphereGeometry = new THREE.SphereGeometry(3, 32, 32);
         var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xF7BD72 });
         var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        sphere.position.set(p.x, p.y, p.z);
+
+        sphere.position.set(firstIntersection.x, 0, firstIntersection.z);
 
         this.meshes.push(sphere);
-        // var position = sphere.position;
-
-        // var p2=new THREE.Vector3(0,100,0);
-
-
-        // var target = p2.add(sphere.position);
-
-        // var t = new TWEEN.Tween(position)
-        // 				.to(target, 2000)
-        // 				.delay(100)
-        // 				.onUpdate(function() {
-        // 					sphere.position.x = position.x;
-        //                     sphere.position.y = position.y;
-        // 				});
-
-        //                  t.start();
         this.r.scene.add(sphere);
     }
 
 
     connect() {
         console.log("Connect!");
-        var material = new THREE.LineBasicMaterial({
-            color: 0xF7BD72
+        this.build(100);
+        this.cleanMeshes();
+    }
+
+    cleanMeshes() {
+        this.meshes.forEach(element => {
+            this.r.scene.remove(element);
         });
 
-        this.build(100);
-
         this.meshes = [];
-
     }
 
     build(height: number) {
-        var rectLength = 20, rectWidth = 20;
-        let g: Array<THREE.Vector3> = new Array<THREE.Vector3>();
-        var v: number[] = [];
-        let index = 0;
 
-        var geometry = new THREE.Geometry();
-
-
-
-        // this.meshes.forEach(element => {
-        //     g.push(element.position);
+        var material = new THREE.MeshLambertMaterial({ color: 0xcccccc });
+        var wallsGeometry = new THREE.Geometry();
 
         for (var i = 0; i < this.meshes.length; i++) {
             let pos1 = this.meshes[i].position;
             let pos2 = i + 1 == this.meshes.length ? this.meshes[0].position : this.meshes[i + 1].position;
 
-            g.push(pos1);
-            g.push(pos2);
-            g.push((new THREE.Vector3(0, height, 0)).add(pos1));
-            g.push((new THREE.Vector3(0, height, 0)).add(pos2));
+            wallsGeometry.vertices.push(pos1);
+            wallsGeometry.vertices.push(pos2);
+            wallsGeometry.vertices.push((new THREE.Vector3(0, height, 0)).add(pos1));
+            wallsGeometry.vertices.push((new THREE.Vector3(0, height, 0)).add(pos2));
         }
 
-        g.forEach(element => {
-            geometry.vertices.push(element);
-        });
-
-        for (var i = 0; i < g.length; i += 4) {
-            geometry.faces.push(new THREE.Face3(i + 2, i + 1, i));
-            geometry.faces.push(new THREE.Face3(i + 2, i + 3, i + 1));
+        for (var i = 0; i < wallsGeometry.vertices.length; i += 4) {
+            wallsGeometry.faces.push(new THREE.Face3(i + 2, i + 1, i));
+            wallsGeometry.faces.push(new THREE.Face3(i + 2, i + 3, i + 1));
 
         }
 
-        var material = new THREE.MeshLambertMaterial({ color: 0xcccccc });
-        geometry.computeFaceNormals();
+        var mesh = new THREE.Mesh(wallsGeometry, material);
 
-        var mesh = new THREE.Mesh(geometry, material);
-
-        var geometry2 =  new THREE.CircleGeometry( 100, this.meshes.length);
-
-        console.log(geometry2);
-
+        var roofGeometry = new THREE.CircleGeometry(100, this.meshes.length);
+        //roofGeometry.vertices.splice(roofGeometry.vertices.length - 1);
         for (var i = 0; i < this.meshes.length; i++) {
-            geometry2.vertices[geometry2.vertices.length - 3 - i] = (new THREE.Vector3(0, height, 0)).add(this.meshes[i].position)
+            roofGeometry.vertices[roofGeometry.vertices.length - 3 - i] = (new THREE.Vector3(0, height, 0)).add(this.meshes[i].position)
         }
 
         var totalX: number = 0;
@@ -156,21 +124,40 @@ class BuildingsController {
             totalY += (this.meshes[i].position.z);
         }
 
-        var c2 = new THREE.Vector3(totalX/ this.meshes.length, height,totalY/this.meshes.length);
+        var midPoint = new THREE.Vector3(totalX / this.meshes.length, height, totalY / this.meshes.length);
 
-        var material2 = new THREE.MeshLambertMaterial({ color: 0xcccccc });
-        geometry2.vertices[geometry2.vertices.length - 1] = c2;
-        geometry2.vertices[geometry2.vertices.length - 2] = (new THREE.Vector3(0, height, 0)).add(this.meshes[0].position);
-        geometry2.computeFaceNormals();
+        roofGeometry.vertices[roofGeometry.vertices.length - 1] = midPoint;
+        //roofGeometry.vertices[roofGeometry.vertices.length - 2] = midPoint;
 
-        var cylinder = new THREE.Mesh(geometry2, material2);
+        roofGeometry.mergeMesh(mesh);
+        roofGeometry.computeFaceNormals();
+        roofGeometry.faceVertexUvs = [];
+
+        var cylinder = new THREE.Mesh(roofGeometry, material);
+
+        cylinder.position.setY(-height);
 
         this.r.scene.add(cylinder)
+        this.moveUp(cylinder, height);
+    }
+
+    moveUp(cylinder: THREE.Object3D, height: number) {
+        var position = cylinder.position;
+
+        var p2 = new THREE.Vector3(0, height, 0);
 
 
-        console.log(mesh);
+        var target = p2.add(cylinder.position);
 
-        this.r.scene.add(mesh);
+        var t = new TWEEN.Tween(position)
+        				.to(target, 2000)
+        				.delay(100)
+        				.onUpdate(function () {
+                cylinder.position.x = position.x;
+                cylinder.position.y = position.y;
+        				});
+
+        t.start();
 
     }
 }
